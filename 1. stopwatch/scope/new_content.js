839789ec31eadeffad8127f2f81a0f36f110b5fc
DEV-RAPHAEL <uniqueinternet2020@gmail.com>
2022-07-28 20:21:47 -0700

    mock stopwatch

--- 1. stopwatch/scope/new_content.js
+++ 1. stopwatch/scope/new_content.js
@@ -10,5 +10,26 @@ document.getElementById('add_love').addEventListener('click', function(){
     so hey how are you + hey how are you ...
 */
 
-/* Now lets try something else */
+/* Now lets try something else  "A mockup timer or stopwatch*/
 
+
+/* by default, start time is ero */
+var sec = 0
+/* variable for the speed  */
+var count;
+/* html elements variables */
+var start = document.getElementById('start')
+var seconds = document.querySelector('.seconds')
+
+/* The button click trigger */
+start.addEventListener('click', function(){
+/* Using conditional statement (if) to check if the button hasnt been clicked */
+   if (!count){
+    count = setInterval(run_seconds, 1000);
+   }
+   /* Main function */
+    function run_seconds (){
+        seconds.textContent = sec + 1
+        sec = sec + 1   
+    }
+})
