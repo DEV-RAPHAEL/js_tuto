4d8c9de2c506a3b3ab94d341a93ae3301a081727
DEV-RAPHAEL <uniqueinternet2020@gmail.com>
2022-07-29 08:17:39 -0700

    Alarm added with no comments

--- 2. Alarm Project/script.js
+++ 2. Alarm Project/script.js
@@ -1,22 +1,31 @@
+/* Set al variables */
 var alarm = document.querySelector(".alarm") 
 var time = document.querySelector(".time") 
 var set_time = document.querySelector(".set_time") 
 
-
+/* Set your alarm prompt using windows.prompt function */
 var a_hour = window.prompt("Set alarm hour")
 var a_minutes = window.prompt("Set alarm minutes")
 var a_seconds = window.prompt("Set alarm seconds")
 
+/* The Main FUnction */
 function current_time(){
+    /* Variable for the new Date() function which checks for date, time, month, year etc */
     const date = new Date();
+    /* Variables for getting hours, minutes, seconds */
     let hours = date.getHours();
     let minute = date.getMinutes();
     let seconds = date.getSeconds();
+
+    /* Setting text using document.queryselector as assigned above in a variable */
     time.textContent = hours +  ':' + minute + ':' +seconds
     set_time.textContent = a_hour +  ':' + a_minutes + ':' + a_seconds
+
+    /* Conditional statement for hours, minutes and seconds */
     if ((hours == a_hour) && (minute == a_minutes) && (seconds == a_seconds)){
         alarm.textContent = "Ding! Ding!! Dong!!!"
     }
+    /* This makes the time keeps counting */
     setTimeout(current_time, 1000)
 }
 
